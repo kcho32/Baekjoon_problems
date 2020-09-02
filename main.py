68b32916f4a672613137fdce77861734e1da05d6
kcho32 <kwdc0302@gmail.com>
2020-09-02 17:54:59 +0900

    9/2 백준 연습문제(실전문제까지 끝)

--- main.py
+++ main.py
@@ -140,8 +140,127 @@ def while_sum_v2_2():
 
 
     return
+    ##EOFError를 이용하라고 했지만, 방법이 뭔지 모르겠다.
+
+
+def sum_cycle():
+    number_i: int = int(input())
+    number_mid = int(number_i)
+    counter: int = 0
+    number: int = 0
+    while True:
+        number = (number_mid%10)*10 + (number_mid//10+number_mid%10)%10
+        number_mid = number
+        counter += 1
+
+        if number == number_i:
+            print(counter)
+            break
+
+    return
+
+
+def cheap_burger():
+    burger_n: int = 3
+    soda_n: int = 2
+    burger_p: list = []
+    soda_p: list = []
+
+    for i in range(burger_n):
+        burger = int(input())
+        burger_p.append(burger)
+    burger_p.sort()
+
+    for j in range(soda_n):
+        soda = int(input())
+        soda_p.append(soda)
+
+    soda_p.sort()
+
+    print(burger_p[0]+soda_p[0]-50)
+
+
+def average_score():
+    student = 5
+    score: int = 0
+    scores: list = []
+
+    for i in range(student):
+        score = int(input())
+        if score<40:
+            score = 40
+        scores.append(score)
+
+    print(int(sum(scores)/student))
+    return
+
+
+def pick_second():
+    numbers: list = sys.stdin.readline().rstrip().split(" ")
+    numbers_int: list = []
+    for i in range(len(numbers)):
+        integers = int(numbers[i])
+        numbers_int.append(integers)
+
+    numbers_int.sort(reverse = True)
+    print(int(numbers_int[1]))
+    return
+
+
+def pick_second_if():
+    numbers: list = sys.stdin.readline().rstrip().split(" ")
+    numbers_int: list = []
+    for i in range(len(numbers)):
+        integers = int(numbers[i])
+        numbers_int.append(integers)
+    if numbers_int[1] <= numbers_int[0] <= numbers_int[2] or numbers_int[2] <= numbers_int[0] <= numbers_int[1]:
+        print(numbers_int[0])
+    elif numbers_int[0] <= numbers_int[1] <= numbers_int[2] or numbers_int[2] <= numbers_int[1] <= numbers_int[0]:
+        print(numbers_int[1])
+    elif numbers_int[0] <= numbers_int[2] <= numbers_int[1] or numbers_int[1] <= numbers_int[2] <= numbers_int[0]:
+        print(numbers_int[2])
+
+
+def put_stars():
+    count: int = int(input())
+    for i in range(count*2-1):
+        if i < count:
+            print("*"*(i+1), sep="", end="\n")
+        elif i >= count:
+            print("*"*(count-(i-count+1)), sep="", end="\n")
+    return
+
+
+def put_stars_v2():
+    number: int = int(input())
+    for i in range(2*number-1):
+        if i < number:
+            print(" "*i+(2*number-2*i-1)*"*")
+        if i >= number:
+            print(" "*(2*number-i-2)+(2*(i-number)+3)*"*")
+
+
+def put_stars_v3():
+    number: int = int(input())
+    for j in range(2*number):
+        if j % 2 == 0:
+            for i in range(number):
+                if i % 2 == 0:
+                    print("*", end="")
+                elif i % 2 !=0:
+                    print(" ", end="")
+        elif j % 2 != 0:
+            for k in range(number):
+                if k % 2 == 0:
+                    print(" ",end="")
+                elif k % 2 != 0:
+                    print("*",end="")
+        print("")
+
+    return
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    while_sum_v2_2()
+    put_stars_v3()
